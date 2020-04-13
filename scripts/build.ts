2db93ff07c28940220a901acd7660a23312ec816
jlk <jianglikui@moego.com>
2020-04-13 20:01:49 +0800

    build Mon Apr 13 2020 20:01:49 GMT+0800 (GMT+08:00)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,26 @@
 async function main() {
+  const branchsString = (await exec("git branch")) as string;
+  const branch = branchsString
+    .split("\n")
+    .filter(line => line.indexOf("*") !== -1)[0]
+    .split(" ")[1];
+
   await exec(`git add .`);
   await exec(`git commit -m "build ${new Date()}"`);
   await exec(`git pull`);
   await exec(`git push`);
   await exec(`git checkout master`);
-  await exec(`git merge develop`);
+  await exec(`git merge ${branch}`);
+  await cleanMasterBranch();
+  await exec(`git pull`);
+  await exec(`git push`);
+  await exec(`git checkout ${branch}`);
+}
+
+function cleanMasterBranch() {
+  return new Promise(resolve => {
+    resolve();
+  });
 }
 
 function exec(script: string) {

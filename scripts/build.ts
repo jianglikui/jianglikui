2b81d23ff7cb853f656a3ecd328943b6bc26fa7e
jlk <jianglikui@moego.com>
2020-04-13 17:53:18 +0800

    build Mon Apr 13 2020 17:53:18 GMT+0800 (GMT+08:00)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,18 @@
-import { format } from "date-fns";
-
 async function main() {
   await exec(`git add .`);
-  await exec(`git commit -m "build ${format(new Date(), "LLLL")}"`);
+  await exec(`git commit -m "build ${new Date()}"`);
 }
 
 function exec(script: string) {
   return new Promise((resolve, reject) => {
     const { exec } = require("child_process");
+    console.log(script);
     exec(script, (err: Error, result: any) => {
       if (err) {
         reject(err);
         return;
       }
+      console.log(result);
       resolve(result);
     });
   });

b0b1603e3a39c6dd529a7aa1b9d938cf4a2087e1
jlk <jianglikui@moego.com>
2020-04-16 14:30:13 +0800

    build Thu Apr 16 2020 14:30:13 GMT+0800 (GMT+08:00)

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -1,27 +1,27 @@
 import { cleanMasterBranch } from "./clearMasterBranch";
-import { exec } from "child_process";
+import { exec } from "./exec";
 
 async function main() {
-  // const branchsString = (await exec("git branch")) as string;
-  // const branch = branchsString
-  //   .split("\n")
-  //   .filter(line => line.indexOf("*") !== -1)[0]
-  //   .split(" ")[1];
+  const branchsString = (await exec("git branch")) as string;
+  const branch = branchsString
+    .split("\n")
+    .filter((line) => line.indexOf("*") !== -1)[0]
+    .split(" ")[1];
 
   await exec(`git status`);
-  // await exec(`git add .`);
-  // await exec(`git commit -m "build ${new Date()}"`);
-  // // await exec(`git pull`);
-  // // await exec(`git push`);
-  // await exec(`git checkout master`);
-  // await exec(`git merge ${branch}`);
+  await exec(`git add .`);
+  await exec(`git commit -m "build ${new Date()}"`);
+  // await exec(`git pull`);
+  // await exec(`git push`);
+  await exec(`git checkout master`);
+  await exec(`git merge ${branch}`);
   cleanMasterBranch({
     path: "./",
     excludes: ["yarn-error.log", ".gitignore", "dist", ".git", "node_modules"],
   });
   // await exec(`git pull`);
   // await exec(`git push`);
-  // await exec(`git checkout ${branch}`);
+  await exec(`git checkout ${branch}`);
 }
 
 main();
